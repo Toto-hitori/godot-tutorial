6d288a6dfc54db1c383ccea14a39e050bd7085fa
Dario <darionuberu@gmail.com>
2023-12-26 18:58:34 +0100

    Added second round of tutorials

---
+++ scenes/Logo.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+var pos = Vector2.ZERO
+const speed = 10
+var test_scale = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pos = Vector2(300,200)
+	position = pos
+	
+#	var test_rotation = 45
+#	rotation_degrees = test_rotation
+	
+	test_scale = 2
+	scale = Vector2(test_scale,test_scale)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pos.x += speed
+	position = pos
+	
+#	test_scale += 1
+#	scale = Vector2(test_scale,test_scale)
---
+++ scenes/level/level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var test_array = ["Test","Hello","stuff"]
+func _ready():
+	$Logo.rotation_degrees = 90
+	for i in test_array:
+		print(i)
+
+func _process(delta):
+	$Logo.rotation_degrees += 10
+	
+	#flow -> if rotation > 180 -> 0
+	if $Logo.rotation_degrees > 180: 
+		$Logo.rotation_degrees = 0
+
+	if $Logo.position.x > 1000:
+		$Logo.pos.x = 0
