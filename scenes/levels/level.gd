3fd7b3c7d6e3b2b60ec3e75072b56594e8d4e020
Dario <darionuberu@gmail.com>
2023-12-28 22:37:05 +0100

    added signals

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -5,3 +5,15 @@ func _ready():
 	for i in test_array:
 		print(i)
 
+
+func _on_gate_player_entered_gate(body):
+	print('gate entered')
+	print(body)
+
+
+func _on_player_laser():
+	print('laser from level')
+
+
+func _on_player_grenade():
+	print('throw grenade from level')
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,29 @@
 extends CharacterBody2D
-
+var can_laser : bool = true
+var can_grenade : bool = true
+signal laser
+signal grenade
 func _process(_delta):
 	var direction = Input.get_vector("Left","Right","Up","Down")
 	velocity = direction  * 500
 	move_and_slide()
-	if(Input.is_action_pressed("Shoot")):
-		print("Shoot")
-	if(Input.is_action_just_pressed("Secondary action")):
-		print("Throw grenade")
+	if(Input.is_action_pressed("Shoot") and can_laser):
+		shoot_laser()
+	if(Input.is_action_pressed("Secondary action") and can_grenade):
+		throw_grenade()
+
+func _on_timer_laser_timeout():
+	can_laser = true
+
+func _on_timer_grenade_timeout():
+	can_grenade = true
+
+func shoot_laser():
+	laser.emit()
+	can_laser = false
+	$TimerLaser.start()
+
+func throw_grenade():
+	grenade.emit()
+	can_grenade = false
+	$GrenadeTimer.start()
