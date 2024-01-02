0f43361a504a9140dc6caa36f6e351e0af94fc7a
Dario <darionuberu@gmail.com>
2024-01-02 18:56:11 +0100

    added inheritance

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -4,3 +4,6 @@ func _process(delta):
 	var direction = Vector2(1,0)
 	velocity = direction * 400
 	move_and_slide()
+	
+func hit():
+	print('damage')
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,11 +1,8 @@
 extends Node2D
+class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-
-func _on_gate_player_entered_gate(_body):
-	var tween = create_tween()
-	tween.tween_property($Player,"speed",0,0.5)
 
 
 func _on_player_laser(pos,direction):
